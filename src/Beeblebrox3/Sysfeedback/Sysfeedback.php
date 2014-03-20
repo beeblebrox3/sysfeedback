bcdddd6150bf1f70d152fa2219b813dda9a2bfce
Luís Henrique <luish.faria@gmail.com>
2014-03-20 09:14:21 -0300

    allow specify format to render

--- src/Beeblebrox3/Sysfeedback/Sysfeedback.php
+++ src/Beeblebrox3/Sysfeedback/Sysfeedback.php
@@ -10,38 +10,59 @@ namespace Beeblebrox3\Sysfeedback;
     Sysfeedback::render(); // show all existing messages
 */
 
-class Sysfeedback {
+class Sysfeedback
+{
     private static $types = array(
         'success',
         'error',
         'warning',
     );
-    public static function render() {
-        foreach(self::$types as $type) {
+
+    private static $format = '<div class="alert alert-:class">:message</div>';
+
+    public static function render($format = '')
+    {
+        foreach (self::$types as $type) {
             $_type = 'sys-'.$type;
-            if(\Session::has($_type)) {
-                self::html($type, \Session::get($_type));
+            if (\Session::has($_type)) {
+                self::html($type, \Session::get($_type), $format);
                 \Session::forget($_type);
             }
         }
     }
 
-    private static function addMessage($type, $message) {
+    private static function addMessage($type, $message)
+    {
         $messages = (array) \Session::get($type);
         $messages[] = $message;
         \Session::put($type, $messages);
     }
 
-    private static function html($class, $messages) {
-        echo '<div class="alert alert-'.$class.'">';
-        foreach($messages as $msg) {
-            echo $msg.'<br />';
-        }
-        echo '</div>';
+    private static function html($class, $messages, $format = '')
+    {
+        $format = ($format) ?: self::$format;
+        $messages = implode('<br />', $messages);
+
+        $output = str_replace(array(
+            ':message',
+            ':class',
+        ), array (
+            $messages,
+            $class
+        ), $format);
+
+        // echo '<div class="alert alert-'.$class.'">';
+        // foreach ($messages as $msg) {
+        //     echo $msg.'<br />';
+        // }
+        // echo '</div>';
+        
+        echo $output;
     }
 
-    public static function __callStatic($type, $args) {
-        if(!in_array($type, self::$types)) {
+    public static function __callStatic($type, $args)
+    {
+        if (!in_array($type, self::$types)) {
             return false;
         }
 
